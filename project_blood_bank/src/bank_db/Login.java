3896b1eeed4e08b04a6a40f31c5af34cfa604890
akashreddych <akashreddychaduvu@my.unt.edu>
2024-01-30 12:02:22 -0600

    new line added

--- project_blood_bank/src/bank_db/Login.java
+++ project_blood_bank/src/bank_db/Login.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+//created the class
 public class Login {
     
     public String id, pass,type;
